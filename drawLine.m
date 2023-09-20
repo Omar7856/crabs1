3a9f0ba24f004a71a68a4d05faa394b5de5234b2
Omar7856 <145413912+Omar7856@users.noreply.github.com>
2023-09-20 13:21:14 -0700

    First Commit

---
+++ drawLine.m
@@ -0,0 +1,21 @@
+%{
+drawLine draws a line from point P to point Q with a color of choice
+%}
+
+function handle = DrawLine (P,Q,myColor)
+
+
+% extracts the x coordinates
+
+x=[P(1) ; Q(1)];
+
+% extracts the y coordinates
+y=[P(2) ; Q(2)];
+%
+handle = plot(x,y,myColor)
+
+% changes the thickness of the line
+
+set(handle, "LineWidth", 3)
+
+endfunction
